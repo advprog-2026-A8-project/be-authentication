16f4ac0b82ef033df3eca1ed6300c16f180e61d3
Jaysen Lestari <lestarijaysen@gmail.com>
2026-02-18 21:52:40 +0700

    init springboot project

---
+++ src/main/java/id/ac/ui/cs/advprog/beauthentication/BeAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.beauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeAuthenticationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/beauthentication/BeAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.beauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
