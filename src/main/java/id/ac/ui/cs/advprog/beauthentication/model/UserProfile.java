e8984b74a30adc0e436aec21e6884dccc4bd99b9
mnaufalmuzaki811 <m.naufal.muzaki811@gmail.com>
2026-04-15 19:43:50 +0700

    fix(auth): hide password from UserProfile API response

--- src/main/java/id/ac/ui/cs/advprog/beauthentication/model/UserProfile.java
+++ src/main/java/id/ac/ui/cs/advprog/beauthentication/model/UserProfile.java
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.advprog.beauthentication.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -23,6 +24,7 @@ public class UserProfile {
     private String email;
 
     @Column(nullable = false)
+    @JsonIgnore
     private String password;
 
     @Column(nullable = false)
