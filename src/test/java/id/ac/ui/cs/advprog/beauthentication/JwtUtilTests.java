a668525d3fcc5de8ce34a37497e8bb7e25c09625
mnaufalmuzaki811 <m.naufal.muzaki811@gmail.com>
2026-05-10 01:16:53 +0700

    fix(lint): resolve checkstyle violations in unit test files

--- src/test/java/id/ac/ui/cs/advprog/beauthentication/JwtUtilTests.java
+++ src/test/java/id/ac/ui/cs/advprog/beauthentication/JwtUtilTests.java
@@ -7,7 +7,11 @@ import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.Date;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class JwtUtilTests {
 
